79a8cceab4524efb014bedf65ae2c6128ae640a9
groos <gegu926@gmial.com>
2024-11-30 00:59:22 +0800

    feat: update style of render to compact

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -3,10 +3,11 @@ return {
   "rcarriga/nvim-notify",
   config = function()
     require("notify").setup({
-      stages = "fade_in_slide_out", -- 设置弹出效果
+      stages = "slide", -- 设置弹出效果
       timeout = 1000, -- 消息显示时间（毫秒）
       max_width = 100,
       background_colour = "#000000",
+      render = "compact",
     })
     vim.notify = require("notify") -- 替换默认的通知功能
   end,
